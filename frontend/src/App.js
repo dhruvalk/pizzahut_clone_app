53bc9acb8f621115043c4e6a22e1c851208e5b35
dhruvalk <kothari.dhruval@gmail.com>
2023-06-09 20:15:33 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen flex-col">
+      <h1 className="text-6xl font-bold">Pizza hut</h1>
+      <span>Landing page</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ resourceserver/src/test/kotlin/com/pizzahut/resourceserver/ResourceserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pizzahut.resourceserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ResourceserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
