84e99a45801162eb466f5f321457f42ae2530d3e
dhruvalk <kothari.dhruval@gmail.com>
2023-07-11 20:05:21 -0400

    updated loading spinner positioning

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function LoadingSpinner() {
   return (
-    <div className="fixed h-screen w-full flex justify-center items-center z-50 backdrop-blur-sm">
+    <div className="fixed h-screen w-screen top-0 left-0 flex justify-center items-center z-50 backdrop-blur-sm">
       <img
         src="https://static.phdvasia.com/sg1/assets/images/icons/loader-bike.gif"
         alt="loading..."
